adbd9c600499d9ac138fdba9f229ecbb91648ca2
Daniel Imfeld <daniel@imfeld.dev>
2022-06-16 23:26:43 -1000

    Add some global middleware

--- api/src/main.rs
+++ api/src/main.rs
@@ -7,6 +7,11 @@ use std::net::{IpAddr, SocketAddr};
 use axum::{Extension, Router};
 use clap::Parser;
 use tower::ServiceBuilder;
+use tower_http::{
+    request_id::MakeRequestUuid,
+    trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer},
+    ServiceBuilderExt,
+};
 use tracing::{event, Level};
 
 use crate::tracing_config::HoneycombConfig;
@@ -29,8 +34,21 @@ async fn main() -> Result<(), anyhow::Error> {
 
     tracing_config::configure(honeycomb_config)?;
 
-    let app =
-        routes::configure_routes(Router::new()).layer(ServiceBuilder::new().layer(Extension(db)));
+    let app = routes::configure_routes(Router::new()).layer(
+        ServiceBuilder::new()
+            .layer(Extension(db))
+            .layer(
+                TraceLayer::new_for_http()
+                    .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                    .on_response(DefaultOnResponse::new().level(Level::INFO))
+                    .on_request(DefaultOnRequest::new().level(Level::INFO)),
+            )
+            .set_x_request_id(MakeRequestUuid)
+            .propagate_x_request_id()
+            .compression()
+            .decompression()
+            .into_inner(),
+    );
 
     let bind_ip: IpAddr = config.host.parse()?;
     let addr = SocketAddr::from((bind_ip, config.port));
