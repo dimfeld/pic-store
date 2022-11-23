8084763982ae2d03753710e023529f58b64a977d
Daniel Imfeld <daniel@imfeld.dev>
2022-11-23 10:38:37 -1000

    Return output image info with base image detail

--- api/src/routes/image/mod.rs
+++ api/src/routes/image/mod.rs
@@ -8,8 +8,10 @@ use axum::{
 };
 use db::{
     base_images,
-    object_id::{BaseImageId, ProjectId, UploadProfileId},
-    upload_profiles, BaseImageStatus, ImageFormat, Permission,
+    conversion_profiles::{ConversionFormat, ConversionSize},
+    object_id::{BaseImageId, OutputImageId, ProjectId, UploadProfileId},
+    output_images, storage_locations, upload_profiles, BaseImageStatus, ImageFormat,
+    OutputImageStatus, Permission, PoolExt,
 };
 use diesel::prelude::*;
 use http::StatusCode;
@@ -20,10 +22,8 @@ use pic_store_db as db;
 use tracing::{event, Level};
 
 use crate::{
-    auth::{Authenticated, UserInfo},
-    get_object, get_object_by_field_query, get_object_query,
-    shared_state::State,
-    Error, Result,
+    auth::Authenticated, get_object_by_field_query, get_object_query, shared_state::State, Error,
+    Result,
 };
 
 #[derive(Debug, Deserialize)]
@@ -131,9 +131,23 @@ async fn get_base_image(
     Authenticated(user): Authenticated,
     Path(image_id): Path<BaseImageId>,
 ) -> Result<impl IntoResponse> {
-    #[derive(Debug, Queryable, Selectable, Serialize)]
+    #[derive(Debug, Queryable, Selectable)]
+    #[diesel(table_name = output_images)]
+    struct OutputImageQueryResult {
+        pub id: OutputImageId,
+        pub location: String,
+        pub width: Option<i32>,
+        pub height: Option<i32>,
+        pub size: ConversionSize,
+        pub format: ConversionFormat,
+
+        pub status: OutputImageStatus,
+        pub updated: chrono::DateTime<chrono::Utc>,
+    }
+
+    #[derive(Debug, Queryable, Selectable)]
     #[diesel(table_name = base_images)]
-    pub struct BaseImageResult {
+    struct BaseImageQueryResult {
         pub id: BaseImageId,
         pub project_id: ProjectId,
         pub hash: Option<String>,
@@ -150,22 +164,146 @@ async fn get_base_image(
         pub updated: chrono::DateTime<chrono::Utc>,
     }
 
-    // TODO Join on output images
-    let (info, allowed) = get_object!(
-        base_images,
-        state,
-        user,
-        BaseImageResult,
-        image_id,
-        db::Permission::ProjectRead
-    )
-    .await?;
-
-    if !allowed {
-        return Err(Error::NotFound);
+    #[derive(Debug, Queryable, Selectable, Serialize)]
+    #[diesel(table_name = storage_locations)]
+    struct StorageLocationInfo {
+        pub provider: db::storage_locations::Provider,
+        pub base_location: String,
+        pub public_url_base: String,
     }
 
-    Ok((StatusCode::OK, Json(info)))
+    let (bst, ost) = diesel::alias!(storage_locations as bst, storage_locations as ost);
+    let (info, base_storage, output_storage, output_images) = state
+        .db
+        .interact(move |conn| {
+            let (info, base_storage, output_storage, allowed) = base_images::table
+                .filter(base_images::id.eq(image_id))
+                .filter(base_images::deleted.is_null())
+                .filter(base_images::team_id.eq(user.team_id))
+                .inner_join(
+                    db::upload_profiles::table
+                        .on(base_images::upload_profile_id.eq(upload_profiles::id))
+                        .inner_join(
+                            bst.on(db::upload_profiles::base_storage_location_id
+                                .eq(bst.field(db::storage_locations::id))),
+                        )
+                        .inner_join(
+                            ost.on(db::upload_profiles::output_storage_location_id
+                                .eq(ost.field(db::storage_locations::id))),
+                        ),
+                )
+                .select((
+                    BaseImageQueryResult::as_select(),
+                    (
+                        bst.field(storage_locations::provider),
+                        bst.field(storage_locations::base_location),
+                        bst.field(storage_locations::public_url_base),
+                    ),
+                    (
+                        ost.field(storage_locations::provider),
+                        ost.field(storage_locations::base_location),
+                        ost.field(storage_locations::public_url_base),
+                    ),
+                    db::obj_allowed!(
+                        user.team_id,
+                        &user.roles,
+                        base_images::project_id.assume_not_null(),
+                        db::Permission::ProjectRead
+                    ),
+                ))
+                .first::<(
+                    BaseImageQueryResult,
+                    StorageLocationInfo,
+                    StorageLocationInfo,
+                    bool,
+                )>(conn)
+                .map_err(Error::from)?;
+
+            if !allowed {
+                return Err(Error::NotFound);
+            }
+
+            let oi = output_images::table
+                .filter(output_images::base_image_id.eq(image_id))
+                .select(OutputImageQueryResult::as_select())
+                .load::<OutputImageQueryResult>(conn)
+                .map_err(Error::from)?;
+
+            Ok((info, base_storage, output_storage, oi))
+        })
+        .await?;
+
+    #[derive(Debug, Serialize)]
+    struct OutputImageResult {
+        pub id: OutputImageId,
+        pub location: String,
+        pub url: String,
+
+        pub width: Option<i32>,
+        pub height: Option<i32>,
+        pub size: ConversionSize,
+        pub format: ConversionFormat,
+
+        pub status: OutputImageStatus,
+
+        pub updated: chrono::DateTime<chrono::Utc>,
+    }
+
+    #[derive(Debug, Serialize)]
+    struct Result {
+        pub id: BaseImageId,
+        pub project_id: ProjectId,
+        pub hash: Option<String>,
+        pub filename: String,
+        pub location: String,
+        pub url: String,
+        pub width: i32,
+        pub height: i32,
+        pub format: Option<ImageFormat>,
+        pub upload_profile_id: UploadProfileId,
+        pub status: BaseImageStatus,
+        pub alt_text: String,
+        pub placeholder: Option<String>,
+
+        pub updated: chrono::DateTime<chrono::Utc>,
+
+        pub output: Vec<OutputImageResult>,
+    }
+
+    let output_images = output_images
+        .into_iter()
+        .map(|o| OutputImageResult {
+            id: o.id,
+            location: format!("{}/{}", output_storage.base_location, o.location),
+            url: format!("{}/{}", output_storage.public_url_base, o.location),
+            width: o.width,
+            height: o.height,
+            size: o.size,
+            format: o.format,
+            status: o.status,
+            updated: o.updated,
+        })
+        .collect::<Vec<_>>();
+
+    let result = Result {
+        id: info.id,
+        project_id: info.project_id,
+        hash: info.hash,
+        filename: info.filename,
+        location: format!("{}/{}", base_storage.base_location, info.location),
+        url: format!("{}/{}", base_storage.public_url_base, info.location),
+        width: info.width,
+        height: info.height,
+        format: info.format,
+        upload_profile_id: info.upload_profile_id,
+        status: info.status,
+        alt_text: info.alt_text,
+        placeholder: info.placeholder,
+        updated: info.updated,
+        output: output_images,
+    };
+
+    Ok((StatusCode::OK, Json(result)))
 }
 
 async fn remove_base_image() -> impl IntoResponse {
